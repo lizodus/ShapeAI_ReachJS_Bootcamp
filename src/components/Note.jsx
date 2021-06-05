79ec29b9941176e63abe54dc007c07a5a31f83f5
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2021-06-05 15:23:07 +0100

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp done by ShapeAI, the instructor was Shaurya Sinha. We covered the
+        basics of Javascript, ReactJS, and
+        HTML.        
+      </p>
+
+      <p> Thank You ShapeAI. </p>
+    </div>
+  );
+}
+
+export default Note;
